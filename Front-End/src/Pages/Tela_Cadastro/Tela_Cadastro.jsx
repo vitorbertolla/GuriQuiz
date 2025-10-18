790bff354093e0ace9245e47d2cbc55ad5e1b6c9
Gabriel Istchuk <gabrielistchuk@alunos.utfpr.edu.br>
2025-10-18 14:35:05 -0300

    estilização tela de login e tela de cadastro

--- Front-End/src/Pages/Tela_Cadastro/Tela_Cadastro.jsx
+++ Front-End/src/Pages/Tela_Cadastro/Tela_Cadastro.jsx
@@ -3,27 +3,29 @@ import styles from './Tela_Cadastro.module.css'
 export default function Cadastro() {
 
     return (
-        <div className={styles.containerCadastro}>
-            <h1 className={styles.title}>CADASTRO</h1>
-            <form className={styles.form}>
-                <div className={styles.field}>
-                    <label className={styles.label}>NICK</label>
-                    <input className={styles.input} type="text" />
-                </div>
-                <div className={styles.field}>
-                    <label className={styles.label}>E-MAIL</label>
-                    <input className={styles.input} type="email" />
-                </div>
-                <div className={styles.field}>
-                    <label className={styles.label}>SENHA</label>
-                    <input className={styles.input} type="password" placeholder='SUA SENHA' />
-                    <input className={styles.input} type="password" placeholder='CONFIRME SUA SENHA' />
-                </div>
-                <div className={styles.actions}>
-                    <button className={styles.botao} type="submit">CADASTRAR</button>
-                    <button className={styles.botao} type="button">LOGIN</button>
-                </div>
-            </form>
+        <div className={styles.container}>
+            <div className={styles.bloco}>
+                <h1 className={styles.title}>CADASTRO</h1>
+                <form className={styles.form}>
+                    <div className={styles.field}>
+                        <label className={styles.label}>NICK</label>
+                        <input className={styles.input} type="text" />
+                    </div>
+                    <div className={styles.field}>
+                        <label className={styles.label}>E-MAIL</label>
+                        <input className={styles.input} type="email" />
+                    </div>
+                    <div className={styles.field}>
+                        <label className={styles.label}>SENHA</label>
+                        <input className={styles.input} type="password" placeholder='SUA SENHA' />
+                        <input className={styles.input} type="password" placeholder='CONFIRME SUA SENHA' />
+                    </div>
+                    <div className={styles.actions}>
+                        <button type="submit"><img className={styles.botao} src="/images/botaoCadastrar.png" alt="" /></button>
+                        <button type="submit"><img className={styles.botao} src="/images/googleCadastrar.png" alt="" /></button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
--- Front-End/src/Pages/Tela_Login/Tela_Login.jsx
+++ Front-End/src/Pages/Tela_Login/Tela_Login.jsx
@@ -2,22 +2,24 @@ import styles from './Tela_Login.module.css'
 
 export default function Login (){
         return(
-            <div className={styles.containerLogin}>
-                <h1 className={styles.title}>LOGIN</h1>
-                <form className={styles.form}>
-                    <div className={styles.field}>
-                        <label className={styles.label}>E-MAIL</label>
-                        <input className={styles.input} type="email" />
+            <div className={styles.container}>
+                <div className={styles.bloco}>
+                    <h1 className={styles.title}>LOGIN</h1>
+                    <form className={styles.form}>
+                        <div className={styles.field}>
+                            <label className={styles.label}>E-MAIL</label>
+                            <input className={styles.input} type="email" />
+                        </div>
+                        <div className={styles.field}>
+                            <label className={styles.label}>SENHA</label>
+                            <input className={styles.input} type="password" placeholder='SUA SENHA' />
+                        </div>
+                    <div className={styles.actions}>
+                        <button type="submit"><img className={styles.botao} src="/images/botaoEntrar.png" alt="" /></button>
+                        <button type="button"><img className={styles.botao} src="/images/googleAcessar.png" alt="" /></button>
                     </div>
-                    <div className={styles.field}>
-                        <label className={styles.label}>SENHA</label>
-                        <input className={styles.input} type="password" placeholder='SUA SENHA' />
-                    </div>
-                <div className={styles.actions}>
-                    <button className={styles.botao} type="submit">Entrar</button>
-                    <button className={styles.botao} type="button">Acessar</button>
+                    </form>
                 </div>
-                </form>
             </div>
         )
 }
