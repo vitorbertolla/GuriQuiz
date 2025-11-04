15ee767b0964a0adf32d1a05d1cf9837877098d4
Gabriel Istchuk <gabrielistchuk@alunos.utfpr.edu.br>
2025-11-04 11:44:24 -0300

    estilização tela start

--- Projeto/src/Pages/Tela_Start/Tela_Start.jsx
+++ Projeto/src/Pages/Tela_Start/Tela_Start.jsx
@@ -2,28 +2,30 @@ import styles from "./Tela_Start.module.css";
 
 export default function Tela_Start() {
   return (
-    <div className="tela-start">
-      <header className="tela-start__header">
-        <div className="tela-start__player">
-          <img className="tela-start__player-image" src="" alt="Imagem jogador" />
-          <h3 className="tela-start__player-name">JOGADOR 1</h3>
-        </div>
-      </header>
+      <div className={styles.container}>
+        <header className={styles.telaStartHeader}>
+          <div className={styles.jogador}>
+            <img className={styles.iconeJogador} src="/images/iconeJogador.png" alt="Imagem jogador" />
+            <h3 className={styles.nomeJogador}>istchuk</h3>
+          </div>
+        </header>
 
-      <main className="tela-start__main">
-        <section className="tela-start__title">
-          <h3 className="tela-start__title-small">
-            <img className="tela-start__title-small-img" src="/images/jogarJuntos.png" alt="" />
+     
+      <main className={styles.containerPrincipal}>
+        <section className={styles.telaStartSection}>
+          <h3>
+            <img img className={styles.tituloPequeno} src="/images/jogarJuntos.png" alt="" />
           </h3>
-          <h1 className="tela-start__title-large">
-            <img className="tela-start__title-large-img" src="/images/tituloGuri.png" alt="" />
+          <h1>
+            <img className={styles.tituloGuri} src="/images/tituloGuri.png" alt="" />
           </h1>
         </section>
 
-        <section className="tela-start__actions">
-          <button className="tela-start__button tela-start__button--primary">Start</button>
+        <section className={styles.botoesContainer}>
+          <button><img className={styles.btnStart} src="/images/botaoStart.png" alt="" /></button>
         </section>
       </main>
     </div>
-  );
+
+  )  
 }
