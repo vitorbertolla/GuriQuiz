a97aa6df2f5e902590f8a9a179296fad65509318
gimenezhiago <163415252+gimenezhiago@users.noreply.github.com>
2025-11-07 14:19:59 -0300

    Arrumando a tela de start

--- Projeto/src/Pages/Tela_Start/Tela_Start.jsx
+++ Projeto/src/Pages/Tela_Start/Tela_Start.jsx
@@ -1,20 +1,42 @@
 import styles from "./Tela_Start.module.css";
+import { useEffect, useState } from "react";
+import { getDoc } from "firebase/firestore";
+import { getUserDocRef } from "../../services/authentication.js";
+import { auth } from "../../services/firebaseConfig"; 
+import { Link } from "react-router-dom";
 
 export default function Tela_Start() {
-  return (
-      <div className={styles.container}>
-        <header className={styles.telaStartHeader}>
-          <div className={styles.jogador}>
-            <img className={styles.iconeJogador} src="/images/iconeJogador.png" alt="Imagem jogador" />
-            <h3 className={styles.nomeJogador}>istchuk</h3>
-          </div>
-        </header>
+  const [nick, setNick] = useState("");
 
-     
+  useEffect(() => {
+    const fetchUserNick = async () => {
+      const user = auth.currentUser; // Obtenha o usuário atual
+      if (user) { // Verifique se existe um usuário logado
+        const userDocRef = getUserDocRef(user.uid);
+        const userSnapshot = await getDoc(userDocRef);
+        if (userSnapshot.exists()) {
+          setNick(userSnapshot.data().nick);
+        }
+      }
+    };
+    fetchUserNick();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <header className={styles.telaStartHeader}>
+        <div className={styles.jogador}>
+          <img className={styles.iconeJogador} src="/images/iconeJogador.png" alt="Imagem jogador" />
+          <h3 className={styles.nomeJogador}>
+            {nick || "Jogador"}
+          </h3>
+        </div>
+      </header>
+
       <main className={styles.containerPrincipal}>
         <section className={styles.telaStartSection}>
           <h3>
-            <img img className={styles.tituloPequeno} src="/images/jogarJuntos.png" alt="" />
+            <img className={styles.tituloPequeno} src="/images/jogarJuntos.png" alt="" />
           </h3>
           <h1>
             <img className={styles.tituloGuri} src="/images/tituloGuri.png" alt="" />
@@ -22,10 +44,13 @@ export default function Tela_Start() {
         </section>
 
         <section className={styles.botoesContainer}>
-          <button><img className={styles.btnStart} src="/images/botaoStart.png" alt="" /></button>
+          <Link to="/menu">
+          <button>
+            <img className={styles.btnStart} src="/images/botaoStart.png" alt="" />
+          </button>
+          </Link>
         </section>
       </main>
     </div>
-
-  )  
+  );
 }
