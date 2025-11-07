eccabc743709254dd797f212050e2dd8488110ed
Gabriel Istchuk <gabrielistchuk@alunos.utfpr.edu.br>
2025-11-07 10:15:13 -0300

    estilização tela de cadastro de perguntas

--- Projeto/src/Pages/Tela_Cadastro_Pergunta/Tela_Cadastro_Pergunta.jsx
+++ Projeto/src/Pages/Tela_Cadastro_Pergunta/Tela_Cadastro_Pergunta.jsx
@@ -69,117 +69,111 @@ export default function Tela_Cadastro_Pergunta({perguntaInicial, onClose, editar
     }
 
     return (
-        <div className={styles['tela-cadastro-pergunta']}>
-            <header className={styles['tela-cadastro-pergunta__header']}>
-                {!editar && (<button className={styles['tela-cadastro-pergunta__exit-button']}>Exit</button>)}
-                <h1 className={styles['tela-cadastro-pergunta__title']}>{editar?"Edição de Perguntas" : "Cadastro de Pergunta"}</h1>
-            </header>
-
-            <main className={styles['tela-cadastro-pergunta__main']}>
-                <form
-                    className={styles['tela-cadastro-pergunta__form']}
-                    onSubmit={submit}
-                >
-                    <div className={styles['tela-cadastro-pergunta__form-row']}>
-                        <input
-                            className={`${styles['tela-cadastro-pergunta__input']} ${styles['tela-cadastro-pergunta__input--pergunta']}`}
-                            type="text"
-                            placeholder="Pergunta"
-                            value={descricao}
-                            onChange={(e) => setDescricao(e.target.value)}
-                        />
-
-                        <select
-                            className={`${styles['tela-cadastro-pergunta__select']} ${styles['tela-cadastro-pergunta__select--dificuldade']}`}
-                            value={dificuldade}
-                            onChange={(e) => setDificuldade(e.target.value)}
-                        >
-                            <option value="">Selecione dificuldade</option>
-                            <option value="facil">Fácil</option>
-                            <option value="medio">Médio</option>
-                            <option value="dificil">Difícil</option>
-                        </select>
-
-                        <select
-                            className={`${styles['tela-cadastro-pergunta__select']} ${styles['tela-cadastro-pergunta__select--materia']}`}
-                            value={materia}
-                            onChange={(e) => setMateria(e.target.value)}
-                        >
-                            <option value="">Selecione matéria</option>
-                            <option value="matematica">Matemática</option>
-                            <option value="portugues">Português</option>
-                            <option value="fisica">Física</option>
-                            <option value="conhecimentosGerais">Conhecimentos Gerais</option>
-                        </select>
-
-                        <button
-                            type="button"
-                            className={`${styles['tela-cadastro-pergunta__button']} ${styles['tela-cadastro-pergunta__button--alternativas']}`}
-                            onClick={() => setModalAberto(true) }   
-                        >
-                            Alternativas
-                        </button>
-
-                        
-                    </div>
-                    {!editar &&(
-                        <div className={styles['tela-cadastro-pergunta__actions']}>
+        <div className={styles.container}>
+            <div className={styles['tela-cadastro-pergunta']}>
+                <header className={styles['tela-cadastro-pergunta__header']}>
+                    {!editar && (<button><img className={styles['tela-cadastro-pergunta__exit-button']} src="/images/botaoExit.png" alt="" ></img></button>)}
+                    <h1 className={styles['tela-cadastro-pergunta__title']}>{editar?"Edição de Perguntas" : "CADASTRO DE PERGUNTAS"}</h1>
+                </header>
+                <main className={styles['tela-cadastro-pergunta__main']}>
+                    <form
+                        className={styles['tela-cadastro-pergunta__form']}
+                        onSubmit={submit}
+                    >
+                        <div className={styles['tela-cadastro-pergunta__form-row']}>
+                            <input
+                                className={`${styles['tela-cadastro-pergunta__input']} ${styles['tela-cadastro-pergunta__input--pergunta']}`}
+                                type="text"
+                                placeholder="PERGUNTA"
+                                value={descricao}
+                                onChange={(e) => setDescricao(e.target.value)}
+                            />
+                            <select
+                                className={`${styles['tela-cadastro-pergunta__select']} ${styles['tela-cadastro-pergunta__select--dificuldade']}`}
+                                value={dificuldade}
+                                onChange={(e) => setDificuldade(e.target.value)}
+                            >
+                                <option value="">DIFICULDADE</option>
+                                <option value="facil">Fácil</option>
+                                <option value="medio">Médio</option>
+                                <option value="dificil">Difícil</option>
+                            </select>
+                            <select
+                                className={`${styles['tela-cadastro-pergunta__select']} ${styles['tela-cadastro-pergunta__select--materia']}`}
+                                value={materia}
+                                onChange={(e) => setMateria(e.target.value)}
+                            >
+                                <option value="">MATERIA</option>
+                                <option value="matematica">Matemática</option>
+                                <option value="portugues">Português</option>
+                                <option value="fisica">Física</option>
+                                <option value="conhecimentosGerais">Conhecimentos Gerais</option>
+                            </select>
                             <button
                                 type="button"
-                                className={`${styles['tela-cadastro-pergunta__button']} ${styles['tela-cadastro-pergunta__button--ai']}`}
+                                className={`${styles['tela-cadastro-pergunta__button']} ${styles['tela-cadastro-pergunta__button--alternativas']}`}
+                                onClick={() => setModalAberto(true) }
                             >
-                                Criar com IA
+                                ALTERNATIVAS
                             </button>
-                            <button
-                                type="submit"
-                                className={`${styles['tela-cadastro-pergunta__button']} ${styles['tela-cadastro-pergunta__button--submit']}`}
-                            >
-                                Cadastrar Pergunta
-                            </button>
+            
                         </div>
-                    )}
-                    {editar && (
-                        <div>
-                            <button type="submit" className={styles['tela-cadastro-pergunta__button']}>
-                                Salvar Alterações
-                            </button>
-                            <button type="button" onClick={onClose} className={styles['tela-cadastro-pergunta__button--cancel']}>
-                            Cancelar
-                            </button>
-                        </div>
-                    )}
-                    {modalAberto && (
-                            <div>
-                                <h3>Alternativas</h3>
-                                <input type="text"  
-                                placeholder='Digite a alternativa A' 
-                                value={alternativaA} onChange={(e) => 
-                                setAlternativaA(e.target.value)} />
-                                <button type='button' onClick={() =>setCorreta("A")}>certa</button>
-                                
-                                <input type="text"  
-                                placeholder='Digite a alternativa B' 
-                                value={alternativaB} onChange={(e) => 
-                                setAlternativaB(e.target.value)} />
-                                <button type='button' onClick={() =>setCorreta("B")}>certa</button>
-
-                                <input type="text"  
-                                placeholder='Digite a alternativa C' 
-                                value={alternativaC} onChange={(e) => 
-                                setAlternativaC(e.target.value)} />
-                                <button type='button' onClick={() =>setCorreta("C")}>certa</button>
-
-                                <input type="text"  
-                                placeholder='Digite a alternativa D' 
-                                value={alternativaD} onChange={(e) => 
-                                setAlternativaD(e.target.value)} />
-                                <button type='button' onClick={() =>setCorreta("D")}>certa</button>
-
-                                <button type='button' onClick={confirmarAlternativas}>Confirmar</button>
+                        {!editar &&(
+                            <div className={styles['tela-cadastro-pergunta__actions']}>
+                                <button
+                                    type="button"
+                                    className={`${styles['tela-cadastro-pergunta__button']} ${styles['tela-cadastro-pergunta__button--ai']}`}
+                                >
+                                    <img className={styles['tela-cadastro-pergunta__btn-criar-com-ia']} src="/images/botaoCriarComIa.png" alt="" ></img>
+                                </button>
+                                <button
+                                    type="submit"
+                                    className={`${styles['tela-cadastro-pergunta__button']} ${styles['tela-cadastro-pergunta__button--submit']}`}
+                                >
+                                    <img className={styles['tela-cadastro-pergunta__btn-cadastrar-pergunta']} src="/images/botaoConfirmar.png" alt="" ></img>
+                                </button>
                             </div>
                         )}
-                </form>
-            </main>
+                        {editar && (
+                            <div>
+                                <button type="submit" className={styles['tela-cadastro-pergunta__button']}>
+                                    Salvar Alterações
+                                </button>
+                                <button type="button" onClick={onClose} className={styles['tela-cadastro-pergunta__button--cancel']}>
+                                Cancelar
+                                </button>
+                            </div>
+                        )}
+                        {modalAberto && (
+                                <div>
+                                    <h3>Alternativas</h3>
+                                    <input type="text"
+                                    placeholder='Digite a alternativa A'
+                                    value={alternativaA} onChange={(e) =>
+                                    setAlternativaA(e.target.value)} />
+                                    <button type='button' onClick={() =>setCorreta("A")}>certa</button>
+            
+                                    <input type="text"
+                                    placeholder='Digite a alternativa B'
+                                    value={alternativaB} onChange={(e) =>
+                                    setAlternativaB(e.target.value)} />
+                                    <button type='button' onClick={() =>setCorreta("B")}>certa</button>
+                                    <input type="text"
+                                    placeholder='Digite a alternativa C'
+                                    value={alternativaC} onChange={(e) =>
+                                    setAlternativaC(e.target.value)} />
+                                    <button type='button' onClick={() =>setCorreta("C")}>certa</button>
+                                    <input type="text"
+                                    placeholder='Digite a alternativa D'
+                                    value={alternativaD} onChange={(e) =>
+                                    setAlternativaD(e.target.value)} />
+                                    <button type='button' onClick={() =>setCorreta("D")}>certa</button>
+                                    <button type='button' onClick={confirmarAlternativas}>Confirmar</button>
+                                </div>
+                            )}
+                    </form>
+                </main>
+            </div>
         </div>
     )
 }
