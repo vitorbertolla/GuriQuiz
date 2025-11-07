945933d3d848abfc6a1452a9f4a600241689e74f
gimenezhiago <163415252+gimenezhiago@users.noreply.github.com>
2025-11-07 14:47:27 -0300

    Arrumei as rotas

--- Projeto/src/Pages/Tela_Menu/Tela_Menu.jsx
+++ Projeto/src/Pages/Tela_Menu/Tela_Menu.jsx
@@ -1,25 +1,56 @@
-import styles from './Tela_Menu.module.css'
+import styles from "./Tela_Menu.module.css";
+import { Link } from "react-router-dom";
 
-export default function Tela_Menu () {
-    return (
-        <div className={styles.container}>
-            <div className={styles.telaMenuMenu}>
-                <header className={styles.telaMenuHeader}>
-                    <button ><img className={styles.telaMenuBtnExit} src="/images/botaoExit.png" alt="" /></button>
-                    <h1><img className={styles.telaMenuTitle} src="/images/tituloMenu.png" alt="" /></h1>
-                </header>
-                <main className={styles.telaMenuMain}>
-                    <section className={styles.telaMenuControls}>
-                        <div className={styles.telaMenuOptions}>
-                            <button><img className={styles.telaMenuBtnOption} src="/images/botaoCadastroPerguntas.png" alt="" /></button>
-                            <button><img className={styles.telaMenuBtnOption} src="/images/botaoRanking.png" alt="" /></button>
-                        </div>
-                        <div className={styles.telaMenuPlay}>
-                            <button><img className={styles.telaMenuBtnPlay} src="/images/botaoJogar.png" alt="" /></button>
-                        </div>
-                    </section>
-                </main>
+export default function Tela_Menu() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.telaMenuMenu}>
+        <header className={styles.telaMenuHeader}>
+          <Link to="/start" className={styles.link}>
+            <img
+              className={styles.telaMenuBtnExit}
+              src="/images/botaoExit.png"
+              alt="Sair"
+            />
+          </Link>
+          <h1>
+            <img
+              className={styles.telaMenuTitle}
+              src="/images/tituloMenu.png"
+              alt="Título do Menu"
+            />
+          </h1>
+        </header>
+        <main className={styles.telaMenuMain}>
+          <section className={styles.telaMenuControls}>
+            <div className={styles.telaMenuOptions}>
+              <Link to="/cadastropergunta" className={styles.link}>
+                <img
+                  className={styles.telaMenuBtnOption}
+                  src="/images/botaoCadastroPerguntas.png"
+                  alt="Cadastrar Perguntas"
+                />
+              </Link>
+              <Link to="/ranking" className={styles.link}>
+                <img
+                  className={styles.telaMenuBtnOption}
+                  src="/images/botaoRanking.png"
+                  alt="Ranking"
+                />
+              </Link>
             </div>
-        </div>
-    )
+            <div className={styles.telaMenuPlay}>
+              <Link to="/configuraçãoQuiz" className={styles.link}>
+                <img
+                  className={styles.telaMenuBtnPlay}
+                  src="/images/botaoJogar.png"
+                  alt="Jogar"
+                />
+              </Link>
+            </div>
+          </section>
+        </main>
+      </div>
+    </div>
+  );
 }
